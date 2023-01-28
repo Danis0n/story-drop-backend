50c194accfece798d5a9c9e39fd64258d1ff04d5
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-01-28 18:02:10 +0300

    sql script for db users

--- sql/user-create.sql
+++ sql/user-create.sql
@@ -1,1 +1,40 @@
--- IN PROGRESS..
+CREATE TABLE role
+(
+    role_id SERIAL NOT NULL PRIMARY KEY UNIQUE,
+    role_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE image
+(
+    image_id UUID NOT NULL PRIMARY KEY UNIQUE,
+    content_type VARCHAR(30) NOT NULL,
+    data BYTEA NOT NULL,
+    image_name VARCHAR(100) NOT NULL,
+    image_size BIGINT NOT NULL
+);
+
+CREATE TABLE sd_user_info
+(
+    info_id SERIAL NOT NULL PRIMARY KEY,
+    text TEXT,
+    contact TEXT
+);
+
+CREATE TABLE sd_user
+(
+    user_id UUID NOT NULL PRIMARY KEY UNIQUE,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    nickname VARCHAR(100) NOT NULL,
+    is_enabled BOOLEAN NOT NULL,
+    is_blocked BOOLEAN NOT NULL,
+    info_id INT REFERENCES sd_user_info(info_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    avatar_id UUID REFERENCES image(image_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE sd_role_user
+(
+    role_id INT NOT NULL REFERENCES role(role_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+    user_id UUID NOT NULL REFERENCES sd_user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
