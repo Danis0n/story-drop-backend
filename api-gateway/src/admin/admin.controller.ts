2e8c532e001478a02992e430e90d2f125e5b4055
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-15 23:09:27 +0300

    fixed permissions

--- api-gateway/src/admin/admin.controller.ts
+++ api-gateway/src/admin/admin.controller.ts
@@ -6,12 +6,15 @@ import {
   OnModuleInit,
   Param,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ADMIN_SERVICE_NAME, AdminServiceClient } from './admin.pb';
 import { ClientGrpc } from '@nestjs/microservices';
 import {
   DeletePostResponseDto,
   DeleteUserResponseDto,
+  RoleGuard,
+  Roles,
   UpdateBannedDto,
   UpdateBannedResponseDto,
   UpdateEnabledDto,
@@ -31,6 +34,8 @@ export class AdminController implements OnModuleInit {
       this.adminClient.getService<AdminServiceClient>(ADMIN_SERVICE_NAME);
   }
 
+  @Roles('Admin')
+  @UseGuards(RoleGuard)
   @Put('ban/:id')
   private async updateBan(
     @Param('id') uuid: string,
@@ -42,6 +47,8 @@ export class AdminController implements OnModuleInit {
     });
   }
 
+  @Roles('Admin')
+  @UseGuards(RoleGuard)
   @Put('enable/:id')
   private async updateEnable(
     @Param('id') uuid: string,
@@ -53,6 +60,8 @@ export class AdminController implements OnModuleInit {
     });
   }
 
+  @Roles('Admin')
+  @UseGuards(RoleGuard)
   @Delete('user/:id')
   private async deleteUser(
     @Param('id') uuid: string,
@@ -60,6 +69,8 @@ export class AdminController implements OnModuleInit {
     return this.adminServiceClient.deleteUser({ uuid: uuid });
   }
 
+  @Roles('Admin')
+  @UseGuards(RoleGuard)
   @Delete('post/:id')
   private async deletePost(
     @Param('id') uuid: string,
