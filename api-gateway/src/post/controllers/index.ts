09a88286630f6e90b51538699b7e9cef020a0cf4
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-22 23:43:22 +0300

    fixed imports

---
+++ api-gateway/src/post/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from './character.controller';
+export * from './fandom.controller';
+export * from './paring.controller';
+export * from './tag.controller';
+export * from './genre.controller';
+export * from './post.controller';
+export * from './collection.controller';
--- api-gateway/src/post/post.module.ts
+++ api-gateway/src/post/post.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { PostController } from './controllers/post.controller';
 import { ClientsModule } from '@nestjs/microservices';
 import { PostServiceProto } from '../common';
 import { AuthModule } from '../auth/auth.module';
+import {
+  CharacterController,
+  CollectionController,
+  FandomController,
+  GenreController,
+  ParingController,
+  PostController,
+  TagController,
+} from './controllers';
 
 @Module({
   imports: [ClientsModule.register([PostServiceProto]), AuthModule],
-  controllers: [PostController],
+  controllers: [
+    PostController,
+    FandomController,
+    CharacterController,
+    CollectionController,
+    TagController,
+    ParingController,
+    GenreController,
+  ],
 })
 export class PostModule {}
