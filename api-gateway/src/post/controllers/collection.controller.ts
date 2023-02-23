b1ca1955bb44bb5a31e39f8333553680227119a4
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-23 13:42:25 +0300

    based character collection methods

--- api-gateway/src/post/controllers/collection.controller.ts
+++ api-gateway/src/post/controllers/collection.controller.ts
@@ -1,6 +1,29 @@
-import { Controller, Inject, OnModuleInit } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  OnModuleInit,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { POST_SERVICE_NAME, PostServiceClient } from '../post.pb';
 import { ClientGrpc } from '@nestjs/microservices';
+import {
+  CreateCollectionRequestDto,
+  CreateCollectionResponseDto,
+  DeleteCollectionResponseDto,
+  FindOneCollectionByIdResponseDto,
+  IsAuthenticatedGuard,
+  RoleGuard,
+  Roles,
+  UpdateCollectionRequestDto,
+  UpdateCollectionResponseDto,
+} from '../../common';
+import { Observable } from 'rxjs';
 
 @Controller('collection')
 export class CollectionController implements OnModuleInit {
@@ -12,5 +35,40 @@ export class CollectionController implements OnModuleInit {
   public onModuleInit(): void {
     this.serviceClient =
       this.client.getService<PostServiceClient>(POST_SERVICE_NAME);
+  }
+
+  @UseGuards(IsAuthenticatedGuard)
+  @Post()
+  private async create(
+    @Body() payload: CreateCollectionRequestDto,
+  ): Promise<Observable<CreateCollectionResponseDto>> {
+    return this.serviceClient.createCollection(payload);
+  }
+
+  @Get('/:id')
+  private async findOneId(
+    @Param('id') uuid: string,
+  ): Promise<Observable<FindOneCollectionByIdResponseDto>> {
+    return this.serviceClient.findOneCollectionById({ collectionId: uuid });
+  }
+
+  @Roles('Admin')
+  @UseGuards(IsAuthenticatedGuard, RoleGuard)
+  @Patch('/:id')
+  private async update(
+    @Param('id') uuid: string,
+    @Body() payload: UpdateCollectionRequestDto,
+  ): Promise<Observable<UpdateCollectionResponseDto>> {
+    payload.collectionId = uuid;
+    return this.serviceClient.updateCollection(payload);
+  }
+
+  @Roles('Admin')
+  @UseGuards(IsAuthenticatedGuard, RoleGuard)
+  @Delete('/:id')
+  private async delete(
+    @Param('id') uuid: string,
+  ): Promise<Observable<DeleteCollectionResponseDto>> {
+    return this.serviceClient.deleteCollection({ collectionId: uuid });
   }
 }
