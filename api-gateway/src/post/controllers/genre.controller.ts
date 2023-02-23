4d13025f4ba84f18330cdff2fdcad42377f9fd54
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-23 14:04:33 +0300

    based fandom genre methods

--- api-gateway/src/post/controllers/genre.controller.ts
+++ api-gateway/src/post/controllers/genre.controller.ts
@@ -1,6 +1,29 @@
-import { Controller, Inject, OnModuleInit } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  OnModuleInit,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { POST_SERVICE_NAME, PostServiceClient } from '../post.pb';
 import { ClientGrpc } from '@nestjs/microservices';
+import {
+  CreateGenreRequestDto,
+  CreateGenreResponseDto,
+  DeleteGenreResponseDto,
+  FindOneGenreByIdResponseDto,
+  IsAuthenticatedGuard,
+  RoleGuard,
+  Roles,
+  UpdateGenreRequestDto,
+  UpdateGenreResponseDto,
+} from '../../common';
+import { Observable } from 'rxjs';
 
 @Controller('genre')
 export class GenreController implements OnModuleInit {
@@ -12,5 +35,40 @@ export class GenreController implements OnModuleInit {
   public onModuleInit(): void {
     this.serviceClient =
       this.client.getService<PostServiceClient>(POST_SERVICE_NAME);
+  }
+
+  @UseGuards(IsAuthenticatedGuard)
+  @Post()
+  private async create(
+    @Body() payload: CreateGenreRequestDto,
+  ): Promise<Observable<CreateGenreResponseDto>> {
+    return this.serviceClient.createGenre(payload);
+  }
+
+  @Get('/:id')
+  private async findOneId(
+    @Param('id') uuid: string,
+  ): Promise<Observable<FindOneGenreByIdResponseDto>> {
+    return this.serviceClient.findOneGenreById({ genreId: uuid });
+  }
+
+  @Roles('Admin')
+  @UseGuards(IsAuthenticatedGuard, RoleGuard)
+  @Patch('/:id')
+  private async update(
+    @Param('id') uuid: string,
+    @Body() payload: UpdateGenreRequestDto,
+  ): Promise<Observable<UpdateGenreResponseDto>> {
+    payload.genreId = uuid;
+    return this.serviceClient.updateGenre(payload);
+  }
+
+  @Roles('Admin')
+  @UseGuards(IsAuthenticatedGuard, RoleGuard)
+  @Delete('/:id')
+  private async delete(
+    @Param('id') uuid: string,
+  ): Promise<Observable<DeleteGenreResponseDto>> {
+    return this.serviceClient.deleteGenre({ genreId: uuid });
   }
 }
