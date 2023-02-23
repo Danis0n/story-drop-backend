af46bdce5ec7c8f0d9d4232b07ded6b7b64cb74d
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-23 13:54:28 +0300

    based fandom collection methods

--- api-gateway/src/post/controllers/fandom.controller.ts
+++ api-gateway/src/post/controllers/fandom.controller.ts
@@ -1,6 +1,29 @@
-import { Controller, Inject, OnModuleInit } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  OnModuleInit,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { POST_SERVICE_NAME, PostServiceClient } from '../post.pb';
 import { ClientGrpc } from '@nestjs/microservices';
+import {
+  CreateFandomRequestDto,
+  CreateFandomResponseDto,
+  DeleteFandomResponseDto,
+  FindOneFandomByIdResponseDto,
+  IsAuthenticatedGuard,
+  RoleGuard,
+  Roles,
+  UpdateFandomRequestDto,
+  UpdateFandomResponseDto,
+} from '../../common';
+import { Observable } from 'rxjs';
 
 @Controller('fandom')
 export class FandomController implements OnModuleInit {
@@ -12,5 +35,40 @@ export class FandomController implements OnModuleInit {
   public onModuleInit(): void {
     this.serviceClient =
       this.client.getService<PostServiceClient>(POST_SERVICE_NAME);
+  }
+
+  @UseGuards(IsAuthenticatedGuard)
+  @Post()
+  private async create(
+    @Body() payload: CreateFandomRequestDto,
+  ): Promise<Observable<CreateFandomResponseDto>> {
+    return this.serviceClient.createFandom(payload);
+  }
+
+  @Get('/:id')
+  private async findOneId(
+    @Param('id') uuid: string,
+  ): Promise<Observable<FindOneFandomByIdResponseDto>> {
+    return this.serviceClient.findOneFandomById({ fandomId: uuid });
+  }
+
+  @Roles('Admin')
+  @UseGuards(IsAuthenticatedGuard, RoleGuard)
+  @Patch('/:id')
+  private async update(
+    @Param('id') uuid: string,
+    @Body() payload: UpdateFandomRequestDto,
+  ): Promise<Observable<UpdateFandomResponseDto>> {
+    payload.fandomId = uuid;
+    return this.serviceClient.updateFandom(payload);
+  }
+
+  @Roles('Admin')
+  @UseGuards(IsAuthenticatedGuard, RoleGuard)
+  @Delete('/:id')
+  private async delete(
+    @Param('id') uuid: string,
+  ): Promise<Observable<DeleteFandomResponseDto>> {
+    return this.serviceClient.deleteFandom({ fandomId: uuid });
   }
 }
