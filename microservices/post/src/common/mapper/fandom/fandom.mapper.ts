f87d9b43a1c2d2bbe205466a735ae1567920f4b9
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-25 12:30:21 +0300

    inited fandom mapper

--- microservices/post/src/common/mapper/fandom/fandom.mapper.ts
+++ microservices/post/src/common/mapper/fandom/fandom.mapper.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { FandomPrisma } from '../../dto/fandom/fandom-prisma.interface';
 
 @Injectable()
-export class FandomMapper {}
+export class FandomMapper {
+  public mapToFandomDto(fandom: FandomPrisma) {
+    return null;
+  }
+}
