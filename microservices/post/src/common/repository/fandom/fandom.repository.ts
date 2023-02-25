b15db054253fd25a3fc3f6fa6d9b2fb0532d3bb1
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-25 12:08:48 +0300

    implemented basee repo methods

--- microservices/post/src/common/repository/fandom/fandom.repository.ts
+++ microservices/post/src/common/repository/fandom/fandom.repository.ts
@@ -1,4 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { FandomPrisma } from '../../dto/fandom/fandom-prisma.interface';
+import { PrismaService } from '../../../prisma/prisma.service';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class FandomRepository {}
+export class FandomRepository {
+  @Inject(PrismaService)
+  private readonly prisma: PrismaService;
+
+  public async create(name: string): Promise<FandomPrisma> {
+    try {
+      return await this.prisma.fandom.create({
+        data: {
+          fandom_id: randomUUID(),
+          fandom_name: name,
+        },
+      });
+    } catch (e) {
+      Logger.error(
+        `create: Ошибка во время создания фандома: ${name}. ${e?.message}`,
+      );
+      return null;
+    }
+  }
+
+  public async update(name: string, fandomId: string): Promise<FandomPrisma> {
+    try {
+      return await this.prisma.fandom.update({
+        where: { fandom_id: fandomId },
+        data: { fandom_name: name },
+      });
+    } catch (e) {
+      Logger.error(
+        `update: Ошибка во время обновлении фандома: ${name}, ${fandomId}. ${e?.message}`,
+      );
+      return null;
+    }
+  }
+
+  public async delete(fandomId: string): Promise<FandomPrisma> {
+    try {
+      return await this.prisma.fandom.delete({
+        where: { fandom_id: fandomId },
+      });
+    } catch (e) {
+      Logger.error(
+        `delete: Ошибка во время удалени фандома: ${fandomId}. ${e?.messagee}`,
+      );
+      return null;
+    }
+  }
+
+  public async findId(fandomId: string): Promise<FandomPrisma> {
+    try {
+      return await this.prisma.fandom.findUnique({
+        where: { fandom_id: fandomId },
+      });
+    } catch (e) {
+      Logger.error(
+        `findId: Ошибка во время поиска фандома по id: ${fandomId}. ${e?.message}`,
+      );
+      return null;
+    }
+  }
+
+  public async findName(name: string): Promise<FandomPrisma> {
+    try {
+      return await this.prisma.fandom.findUnique({
+        where: { fandom_name: name },
+      });
+    } catch (e) {
+      Logger.error(
+        `findName: Ошибка во время поиска фандома по имени: ${name}. ${e}`,
+      );
+      return null;
+    }
+  }
+
+  public async findNameMany(name: string): Promise<FandomPrisma[]> {
+    try {
+      return await this.prisma.fandom.findMany({
+        where: { fandom_name: { contains: name, mode: 'insensitive' } },
+      });
+    } catch (e) {
+      Logger.error(
+        `findNameMany: Ошибка во время поиска фандомов по имени: ${name}. ${e}`,
+      );
+      return null;
+    }
+  }
+}
