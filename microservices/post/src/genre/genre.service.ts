9cfd044110217aa3ec6fb8923d38b553da86207d
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-25 18:43:20 +0300

    implemented base methods

--- microservices/post/src/genre/genre.service.ts
+++ microservices/post/src/genre/genre.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import {
   CreateGenreRequestDto,
   CreateGenreResponseDto,
@@ -6,34 +6,64 @@ import {
   DeleteGenreResponseDto,
   FindOneGenreByIdRequestDto,
   FindOneGenreByIdResponseDto,
+  GenreMapper,
+  GenreRepository,
   UpdateGenreRequestDto,
   UpdateGenreResponseDto,
 } from '../common';
+import {
+  GrpcAlreadyExistsException,
+  GrpcNotFoundException,
+} from 'nestjs-grpc-exceptions';
 
 @Injectable()
 export class GenreService {
+  @Inject(GenreRepository)
+  private readonly repository: GenreRepository;
+
+  @Inject(GenreMapper)
+  private readonly mapper: GenreMapper;
+
   public async create({
     name,
   }: CreateGenreRequestDto): Promise<CreateGenreResponseDto> {
-    return { genre: null, success: false };
+    const genre = await this.repository.create(name);
+    if (!genre)
+      throw new GrpcAlreadyExistsException(
+        'Жанр с таким именем уже сущесвтует!',
+      );
+
+    return { genre: this.mapper.mapToGenreDto(genre), success: true };
   }
 
   public async findId({
     genreId,
   }: FindOneGenreByIdRequestDto): Promise<FindOneGenreByIdResponseDto> {
-    return { genre: null, success: false };
+    const genre = await this.repository.findId(genreId);
+    if (!genre)
+      throw new GrpcNotFoundException('Жанр с таким id не сущесвтует!');
+
+    return { genre: this.mapper.mapToGenreDto(genre), success: true };
   }
 
   public async update({
     genreId,
     name,
   }: UpdateGenreRequestDto): Promise<UpdateGenreResponseDto> {
-    return { genre: null, success: false };
+    const genre = await this.repository.update(name, genreId);
+    if (!genre)
+      throw new GrpcNotFoundException('Жанр с таким id не сущесвтует!');
+
+    return { genre: this.mapper.mapToGenreDto(genre), success: true };
   }
 
   public async delete({
     genreId,
   }: DeleteGenreRequestDto): Promise<DeleteGenreResponseDto> {
-    return { success: false };
+    const genre = await this.repository.delete(genreId);
+    if (!genre)
+      throw new GrpcNotFoundException('Жанр с таким id не сущесвтует!');
+
+    return { success: true };
   }
 }
