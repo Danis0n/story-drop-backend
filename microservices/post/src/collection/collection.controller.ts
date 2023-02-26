adb358e4454fc851a555bcb757667567c62d7f1a
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-26 14:10:57 +0300

    implemented based methods

--- microservices/post/src/collection/collection.controller.ts
+++ microservices/post/src/collection/collection.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { POST_SERVICE_NAME } from '../post/proto/post.pb';
+import { CollectionService } from './collection.service';
+import {
+  CreateCollectionRequestDto,
+  CreateCollectionResponseDto,
+  DeleteCollectionRequestDto,
+  DeleteCollectionResponseDto,
+  FindOneCollectionByIdRequestDto,
+  FindOneCollectionByIdResponseDto,
+  UpdateCollectionRequestDto,
+  UpdateCollectionResponseDto,
+} from '../common';
 
 @Controller('collection')
-export class CollectionController {}
+export class CollectionController {
+  @Inject(CollectionService)
+  private readonly service: CollectionService;
+
+  @GrpcMethod(POST_SERVICE_NAME, 'CreateCollection')
+  private async create(
+    payload: CreateCollectionRequestDto,
+  ): Promise<CreateCollectionResponseDto> {
+    return this.service.create(payload);
+  }
+
+  @GrpcMethod(POST_SERVICE_NAME, 'FindOneCollectionById')
+  private async findId(
+    payload: FindOneCollectionByIdRequestDto,
+  ): Promise<FindOneCollectionByIdResponseDto> {
+    return this.service.findId(payload);
+  }
+
+  @GrpcMethod(POST_SERVICE_NAME, 'UpdateCollection')
+  private async update(
+    payload: UpdateCollectionRequestDto,
+  ): Promise<UpdateCollectionResponseDto> {
+    return this.service.update(payload);
+  }
+
+  @GrpcMethod(POST_SERVICE_NAME, 'DeleteCollection')
+  private async delete(
+    payload: DeleteCollectionRequestDto,
+  ): Promise<DeleteCollectionResponseDto> {
+    return this.service.delete(payload);
+  }
+}
--- microservices/post/src/collection/collection.service.ts
+++ microservices/post/src/collection/collection.service.ts
@@ -1,4 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  CollectionMapper,
+  CollectionRepository,
+  CreateCollectionRequestDto,
+  CreateCollectionResponseDto,
+  DeleteCollectionRequestDto,
+  DeleteCollectionResponseDto,
+  FindOneCollectionByIdRequestDto,
+  FindOneCollectionByIdResponseDto,
+  UpdateCollectionRequestDto,
+  UpdateCollectionResponseDto,
+} from '../common';
+import {
+  GrpcInternalException,
+  GrpcNotFoundException,
+} from 'nestjs-grpc-exceptions';
+
+// TODO: add create with posts
+// TODO: add add post to collection_post
+// TODO: add delete post from collection_post
 
 @Injectable()
-export class CollectionService {}
+export class CollectionService {
+  @Inject(CollectionMapper)
+  private readonly mapper: CollectionMapper;
+
+  @Inject(CollectionRepository)
+  private readonly repository: CollectionRepository;
+
+  public async findId({
+    collectionId,
+  }: FindOneCollectionByIdRequestDto): Promise<FindOneCollectionByIdResponseDto> {
+    const collection = await this.repository.findId(collectionId);
+    if (!collection)
+      throw new GrpcNotFoundException('Коллекция с таким id не найдена!');
+
+    return {
+      collection: this.mapper.mapToCollectionDto(collection),
+      success: true,
+    };
+  }
+
+  public async delete({
+    collectionId,
+  }: DeleteCollectionRequestDto): Promise<DeleteCollectionResponseDto> {
+    const collection = await this.repository.delete(collectionId);
+    if (!collection)
+      throw new GrpcNotFoundException('Коллекция с таким id не найдена!');
+
+    return { success: true };
+  }
+
+  public async update({
+    name,
+    collectionId,
+  }: UpdateCollectionRequestDto): Promise<UpdateCollectionResponseDto> {
+    const collection = await this.repository.update(
+      collectionId,
+      name,
+      null,
+      null,
+      null,
+    );
+    if (!collection)
+      throw new GrpcNotFoundException('Коллекция с таким id не найдена!');
+
+    return {
+      collection: this.mapper.mapToCollectionDto(collection),
+      success: true,
+    };
+  }
+
+  public async create({
+    name,
+    userId,
+  }: CreateCollectionRequestDto): Promise<CreateCollectionResponseDto> {
+    const collection = await this.repository.create(name, userId);
+    if (!collection)
+      throw new GrpcInternalException('Ошибка при создании коллекции!');
+
+    return {
+      collection: this.mapper.mapToCollectionDto(collection),
+      success: false,
+    };
+  }
+}
