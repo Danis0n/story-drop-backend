df04e4103e9c459f52e57066f070eb9c56a344fe
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-27 13:45:12 +0300

    added collection mapper defualt

--- microservices/post/src/common/mapper/collection/collection.mapper.ts
+++ microservices/post/src/common/mapper/collection/collection.mapper.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { CollectionPrisma } from '../../dto/collection/collection-prisma.interface';
+import {
+  CollectionPostPrisma,
+  CollectionPrisma,
+} from '../../dto/collection/collection-prisma.interface';
+import { CollectionDto } from '../../dto';
 
 @Injectable()
 export class CollectionMapper {
-  public mapToCollectionDto(collection: CollectionPrisma) {
+  public mapToCollectionDto(collection: CollectionPrisma): CollectionDto {
     return null;
+  }
+
+  private mapToPrismaPostId(postId: string): CollectionPostPrisma {
+    return { post_id: postId };
+  }
+
+  public mapToPrismaPostIds(postIds: string[]): CollectionPostPrisma[] {
+    return postIds.map((postId) => {
+      return this.mapToPrismaPostId(postId);
+    });
   }
 }
