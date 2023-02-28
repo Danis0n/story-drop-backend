b55ee085f8541c034a002034e1ba91d51efd6ab5
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-28 23:10:24 +0300

    implemented base methods

--- microservices/post/src/tag/tag.controller.ts
+++ microservices/post/src/tag/tag.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { POST_SERVICE_NAME } from '../post/proto/post.pb';
+import {
+  CreateTagRequestDto,
+  CreateTagResponseDto,
+  DeleteTagRequestDto,
+  DeleteTagResponseDto,
+  FindOneTagByIdRequestDto,
+  FindOneTagByIdResponseDto,
+  UpdateTagRequestDto,
+  UpdateTagResponseDto,
+} from '../common';
+import { TagService } from './tag.service';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+  @Inject(TagService)
+  private readonly service: TagService;
+
+  @GrpcMethod(POST_SERVICE_NAME, 'CreateTag')
+  private async create(
+    payload: CreateTagRequestDto,
+  ): Promise<CreateTagResponseDto> {
+    return this.service.create(payload);
+  }
+
+  @GrpcMethod(POST_SERVICE_NAME, 'FindOneTagById')
+  private async findId(
+    payload: FindOneTagByIdRequestDto,
+  ): Promise<FindOneTagByIdResponseDto> {
+    return this.service.findId(payload);
+  }
+
+  @GrpcMethod(POST_SERVICE_NAME, 'UpdateTag')
+  private async update(
+    payload: UpdateTagRequestDto,
+  ): Promise<UpdateTagResponseDto> {
+    return this.service.update(payload);
+  }
+
+  @GrpcMethod(POST_SERVICE_NAME, 'DeleteTag')
+  private async delete(
+    payload: DeleteTagRequestDto,
+  ): Promise<DeleteTagResponseDto> {
+    return this.service.delete(payload);
+  }
+}
--- microservices/post/src/tag/tag.service.ts
+++ microservices/post/src/tag/tag.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  CreateTagRequestDto,
+  CreateTagResponseDto,
+  DeleteTagRequestDto,
+  DeleteTagResponseDto,
+  FindOneTagByIdRequestDto,
+  FindOneTagByIdResponseDto,
+  TagMapper,
+  TagRepository,
+  UpdateTagRequestDto,
+  UpdateTagResponseDto,
+} from '../common';
+import {
+  GrpcInvalidArgumentException,
+  GrpcNotFoundException,
+} from 'nestjs-grpc-exceptions';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+  @Inject(TagRepository)
+  private readonly repository: TagRepository;
+
+  @Inject(TagMapper)
+  private readonly mapper: TagMapper;
+
+  public async create({
+    name,
+    ageId,
+  }: CreateTagRequestDto): Promise<CreateTagResponseDto> {
+    const tag = await this.repository.create(name, ageId);
+    if (!tag)
+      throw new GrpcInvalidArgumentException('Ошибка при создании тэга!');
+
+    return { success: true, tag: this.mapper.mapToTagDto(tag) };
+  }
+
+  public async findId({
+    tagId,
+  }: FindOneTagByIdRequestDto): Promise<FindOneTagByIdResponseDto> {
+    const tag = await this.repository.findId(tagId);
+    if (!tag) throw new GrpcNotFoundException('Ошибка при поиске тэга!');
+
+    return { success: true, tag: this.mapper.mapToTagDto(tag) };
+  }
+
+  public async update({
+    name,
+    tagId,
+  }: UpdateTagRequestDto): Promise<UpdateTagResponseDto> {
+    const tag = await this.repository.update(tagId, name);
+    if (!tag) throw new GrpcNotFoundException('Ошибка при обновлении тэга!');
+
+    return { success: true, tag: this.mapper.mapToTagDto(tag) };
+  }
+
+  public async delete({
+    tagId,
+  }: DeleteTagRequestDto): Promise<DeleteTagResponseDto> {
+    const tag = await this.repository.delete(tagId);
+    if (!tag) throw new GrpcNotFoundException('Ошибка при удалении тэга!');
+
+    return { success: true };
+  }
+}
