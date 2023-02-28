894a4905feb13946c3bd6900ecdf68aa1bd77ed8
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-02-28 23:09:08 +0300

    implemented base methods

---
+++ microservices/post/src/common/mapper/tag/tag.mapper.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { AgeDto, InsertAge, TagDto } from '../../dto';
+import { TagWithRelation } from '../../validation';
+import { Builder } from 'builder-pattern';
+
+@Injectable()
+export class TagMapper {
+  public mapToTagDto({ tag_id, tag_name, age }: TagWithRelation): TagDto {
+    return Builder(TagDto)
+      .tagId(tag_id)
+      .name(tag_name)
+      .age(
+        Builder(AgeDto)
+          .ageId(age.age_id)
+          .name(age.age_name)
+          .color(age.color)
+          .build(),
+      )
+      .build();
+  }
+
+  private mapToInsertAge(age: string): InsertAge {
+    return { age_id: age };
+  }
+}
