3add7e8576f85c866415c25dbaeacb3d0c999e7e
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-03-02 15:09:54 +0300

    fixed create-update params in request

--- api-gateway/src/common/dto/chapter/request.dto.ts
+++ api-gateway/src/common/dto/chapter/request.dto.ts
@@ -1,23 +1,33 @@
 import {
   CreateChapterRequest,
-  UpdateChapterRequest,
   DeleteChapterRequest,
   FindOneChapterByIdRequest,
+  UpdateChapterRequest,
 } from '../../../modules/post/post.pb';
+import { IsOptional } from 'class-validator';
 
 export class CreateChapterRequestDto implements CreateChapterRequest {
   name: string;
+  @IsOptional()
   notes: string;
   postId: string;
+  @IsOptional()
   text: string;
   userId: string;
+  @IsOptional()
+  isHidden: boolean;
 }
 
 export class UpdateChapterRequestDto implements UpdateChapterRequest {
+  name: string;
   chapterId: string;
+  @IsOptional()
   notes: string;
+  @IsOptional()
   text: string;
   userId: string;
+  @IsOptional()
+  isHidden: boolean;
 }
 
 export class DeleteChapterRequestDto implements DeleteChapterRequest {
